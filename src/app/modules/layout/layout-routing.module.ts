2ae1d81500186fc4888418ec19c2cdcbc3ae33ef
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-09-29 12:14:01 +0200

    bullsiht

---
+++ src/app/modules/layout/layout-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: LayoutComponent
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'error/404' },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {}
