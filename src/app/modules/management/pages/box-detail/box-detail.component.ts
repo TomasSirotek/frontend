9b9149dced33b2ca24c6bf6708b5880c5a2f2c8e
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-09-30 13:06:14 +0200

    delete functionality prep

--- src/app/modules/management/pages/box-detail/box-detail.component.ts
+++ src/app/modules/management/pages/box-detail/box-detail.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Box } from '../../models/box';
 import { BoxServiceService } from '../../services/box-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BoxesHeaderComponent } from '../../components/boxes/boxes-header/boxes-header.component';
+import { ConfirmModalComponent } from 'src/app/shared/components/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-box-detail',
   standalone: true,
-  imports: [CommonModule,BoxesHeaderComponent],
+  imports: [CommonModule,BoxesHeaderComponent,ConfirmModalComponent],
   templateUrl: './box-detail.component.html',
   styleUrls: ['./box-detail.component.scss']
 })
 export class BoxDetailComponent {
+
   box: Box; 
   id: number;
 
   constructor(
     private route: ActivatedRoute,
-    private boxService: BoxServiceService
+    private boxService: BoxServiceService,
+    private router: Router // Add this line
   ) {}
 
   ngOnInit(): void {
@@ -30,5 +33,17 @@ export class BoxDetailComponent {
     // this.boxService.getBoxById(boxId).subscribe((data) => {
     //   this.box = data; 
     // });
+  }
+
+  openModal() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+      document.getElementById('deleteButton').click();
+    });
+  }
+
+  // TODO: Do validaiton and impletnt delete service to the http client
+  deleteItem() {
+    console.log("delete item" + this.id)
+    this.router.navigate(['/management/boxes']);
   }
 }
