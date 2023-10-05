63806f956662cfe0d4fbda1a904088037e961e36
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-05 16:01:20 +0200

    done with front end

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-     BASE_URL: 'https://factory-api-service.azurewebsites.net/api'
-    // BASE_URL: 'http://localhost:5000/api',
+    BASE_URL:'http://localhost:5000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
     BASE_URL: 'https://factory-api-service.azurewebsites.net/api',
-    // BASE_URL:'http://localhost:5000/api'
 };
